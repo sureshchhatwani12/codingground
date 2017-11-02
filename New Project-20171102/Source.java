9a8fe0dc55e4a42498bd3dafa455ad2dbd512dab
Suresh Chhatwani <31374067+sureshchhatwani12@users.noreply.github.com>
2017-11-02 16:01:47 -0400

    Created by Coding Ground Client

---
+++ New Project-20171102/Source.java
@@ -0,0 +1,9 @@
+public class Source{
+
+     public static void main(String []args){
+        double a[]={5.0,10.0,15.0,20.0,35.0};
+        System.out.println("Calling Statistics Class");
+        Statistics.average(a);
+        System.out.println("Called Statistics Class");
+     }
+}
